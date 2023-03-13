250965abf34c2bc82260a080000bafaf3d6c7068
ertug-guney <yukselenay@gmail.com>
2023-03-13 23:05:01 +0300

    3. commit

--- sqlOdev3.sql
+++ sqlOdev3.sql
@@ -5,7 +5,7 @@ SELECT country FROM country WHERE country LIKE 'A%a';
 SELECT country FROM country WHERE country LIKE '_____%n';
 
 --3. film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
-SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
+SELECT title FROM film WHERE title ~~* '%t%t%t%t%';
 
 --4. film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
 SELECT * FROM film WHERE title ILIKE 'C%' AND length > 90 AND rental_rate =2.99 ;
